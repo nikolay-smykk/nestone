cb17eff5ce69cec26366388b8caec5119a91581e
Nikolay Smyk <nikolaysmyk3@gmail.com>
2022-05-10 17:29:23 +0300

    base

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypegooseModule } from 'nestjs-typegoose';
+import { getMongoConfig } from './config/mongo.config';
 
 @Module({
   imports: [
@@ -17,7 +18,7 @@ import { TypegooseModule } from 'nestjs-typegoose';
       useFactory: getMongoConfig,
     }),
     AuthModule,
-    TopPageModule,
+    // TopPageModule,
     ProductModule,
     ReviewModule,
   ],
--- src/config/mongo.config.ts
+++ src/config/mongo.config.ts
@@ -10,13 +10,20 @@ export const getMongoConfig = async (
   };
 };
 
-const getMongoString = (configService: ConfigService): string => {
-  return `mongodb://${configService.get('MONGO_LOGIN')}:${configService.get(
+const getMongoString = (configService: ConfigService) =>
+  `mongodb://${configService.get('MONGO_LOGIN')}:${configService.get(
     'MONGO_PASSWORD',
   )}@${configService.get('MONGO_HOST')}:${configService.get(
     'MONGO_PORT',
   )}/${configService.get('MONGO_AUTH_DB')}`;
-};
+
+// const getMongoString = (configService: ConfigService) =>
+//   'mongodb://' +
+//   configService.get('MONGO_HOST') +
+//   ':' +
+//   configService.get('MONGO_PORT') +
+//   '/' +
+//   configService.get('MONGO_AUTH_DB');
 
 const getMongoOptions = (): any => {
   useNewUrlParser: true;
